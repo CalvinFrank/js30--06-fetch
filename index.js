3160e71042ce09294b1e5fd620b4f8a8817b3022
Calvin Frank <Calvin Anderson Frank>
2022-06-22 09:48:43 -0400

    done with challenge

--- index.js
+++ index.js
@@ -10,17 +10,19 @@ function findMatches(wordToMatch, cities) {
         return place.city.match(regex) || place.state.match(regex)
     })
 }
-
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+}
 function displayMatches() {
     const matchesArray = findMatches(this.value, cities)
     const html = matchesArray.map(place => {
-        // const regex = new RegExp(this.value, "gi")
-        // const cityName = place.city.replace(regex, `<span class="h1">${this.value}</span>`)
-        // const stateName = place.state.replace(regex, `<span class="h1">${this.value}</span>`)
+        const regex = new RegExp(this.value, "gi")
+        const cityName = place.city.replace(regex, `<span class="h1">${this.value}</span>`)
+        const stateName = place.state.replace(regex, `<span class="h1">${this.value}</span>`)
         return `
             <li>
-                <span class="name">${place.city} ${place.state}</span>
-                <span class="population">${place.population}</span>
+                <span class="name">${cityName} ${place.state}</span>
+                <span class="population">${numberWithCommas(place.population)}</span>
             </li>
         `
     }).join("")
